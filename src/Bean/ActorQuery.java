cc060b7587eba09c7de8c97f98738444182bd9e3
覃庆显 <971252914@qq.com>
2018-06-18 11:24:56 +0800

    comlete the actorQuery and directorquery bean func

--- src/Bean/ActorQuery.java
+++ src/Bean/ActorQuery.java
@@ -6,12 +6,43 @@ public class ActorQuery {
     private String PersonName;
     private String PersonBirth;
 
-    public ActorQuery(String Role, String FilmName, String PersonName, String PersonBirth)
-    {
+    public ActorQuery(String Role, String FilmName, String PersonName, String PersonBirth) {
         this.Role = Role;
         this.FilmName = FilmName;
         this.PersonName = PersonName;
         this.PersonBirth = PersonBirth;
     }
 
+    public String getRole() {
+        return Role;
+    }
+
+
+    public String getFilmName() {
+        return FilmName;
+    }
+
+    public String getPersonBirth() {
+        return PersonBirth;
+    }
+
+    public String getPersonName() {
+        return PersonName;
+    }
+
+    public void setRole(String role) {
+        Role = role;
+    }
+
+    public void setFilmName(String filmName) {
+        FilmName = filmName;
+    }
+
+    public void setPersonBirth(String personBirth) {
+        PersonBirth = personBirth;
+    }
+
+    public void setPersonName(String personName) {
+        PersonName = personName;
+    }
 }
